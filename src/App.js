f9c628962a67aea72f75b54bec7eb02fb679329e
unknown <norefice45@gmail.com>
2024-01-23 09:11:43 +0100

    menu classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-
+import ClassesMenu from "./components/js/classes";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Lofi-RPG</h1>
       </header>
+      <div className="game-board">
+        <ClassesMenu />
+
+      </div>
     </div>
   );
 }
